982160ee3628052e116692b911b0eefe2d90536b
Dimitry Ignatev <cutecutely@mail.ru>
2021-10-24 13:53:59 +0500

    зеленая форма и без кнопки закрыть

--- MyNotepad/Form1.cs
+++ MyNotepad/Form1.cs
@@ -16,10 +16,5 @@ namespace MyNotepad
         {
             InitializeComponent();
         }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            Close();
-        }
     }
 }
